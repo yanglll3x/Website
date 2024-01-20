7b8fed01786f62f2a07586ba66177b68b92dd562
yanglll3x <yanglll3x@gmail.com>
2024-01-20 18:35:08 +1100

    update_pearl

--- src/pages/Work_explainer.jsx
+++ src/pages/Work_explainer.jsx
@@ -4,7 +4,6 @@ import pearlVideo1 from '../assets/images/pearlVideo1.png';
 import pearlVideo2 from '../assets/images/pearlVideo2.png';
 import pearlVideo3 from '../assets/images/pearlVideo3.png';
 import pearlVideo4 from '../assets/images/pearlVideo4.png';
-import testImg from '../assets/images/test.png';
 
 
 const Work_explainer = () => {
@@ -14,8 +13,8 @@ const Work_explainer = () => {
       <div className='flex flex-col w-full items-center'>
         <img src={pearlVideo1} alt='ExplainVideoImage1' className='w-full h-auto' />
         <img src={pearlVideo2} alt='ExplainVideoImage2' className='w-full h-auto' />
-        <img src={pearlVideo2} alt='ExplainVideoImage3' className='w-full h-auto' />
-        <img src={pearlVideo2} alt='ExplainVideoImage4' className='w-full h-auto' />
+        <img src={pearlVideo3} alt='ExplainVideoImage3' className='w-full h-auto' />
+        <img src={pearlVideo4} alt='ExplainVideoImage4' className='w-full h-auto' />
       </div>
 
     </section>
